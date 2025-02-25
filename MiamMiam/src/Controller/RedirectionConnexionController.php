4fac5ebee4a192c4a19df5239e1fc6c8d8790e5e
Milanor38 <65493965+Milanor38@users.noreply.github.com>
2025-02-25 18:58:15 +0100

    Création et avancement page isncription

---
+++ MiamMiam/src/Controller/RedirectionConnexionController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class RedirectionConnexionController extends AbstractController{
+    #[Route('/login', name:'login_page')]
+    public function loginPage(){
+        return $this->render('utilisateur/login.html.twig');
+    }
+
+  
+}
